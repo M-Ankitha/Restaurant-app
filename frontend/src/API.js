e4810636201bc3c591f99f7df9070341bd7729c9
M-Ankitha <122814756+M-Ankitha@users.noreply.github.com>
2025-12-03 16:40:29 +0100

    Update API base URL configuration

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || " https://restaurant-app-z9ol.onrender.com",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 export const setAuthToken = (token) => {
